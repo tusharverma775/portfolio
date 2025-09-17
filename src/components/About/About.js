0fdeb7d062957359caa0ee634942edfc25f60148
tusharverma77 <tusharverma775@gmail.com>
2025-09-17 11:28:05 +0530

    something

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
+// import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
@@ -44,8 +44,8 @@ function About() {
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
-
-        <Github />
+{/* 
+        <Github /> */}
       </Container>
     </Container>
   );
